9676fe74637650aa23228468c8fc7535507931ef
Kadin <KMD@XWulf.local>
2020-07-29 19:40:17 -0700

    added node_modules to gitignore

--- contracts/Upstate.sol
+++ contracts/Upstate.sol
@@ -2,22 +2,24 @@
 // contracts/Upstate.sol
 pragma solidity >=0.5.0 <0.7.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Upstate is ERC20 {
     uint256 private _startTime;
     uint256 private _endTime;
+    uint256 public createTime;
 
-    constructor(uint256 startTime, uint256 endTime) ERC20("Upstate", "Up") public {
+    constructor(uint256 startTime, uint256 endTime) ERC20('Upstate', 'Up') public {
         _mint(msg.sender, 1000);
         _startTime = startTime;
         _endTime = endTime;
     }
 
-    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        if (now > _startTime && now < _endTime) {
-            _transfer(_msgSender(), recipient, amount);
-        }  
-        return true;
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+        super._beforeTokenTransfer(from, to, amount); 
+        uint256 time = block.timestamp;
+        require(time >= _startTime, "can only send transactions after start time");
+        // require(time <= _startTime, "can only send transactions before end time");  
     }
+    
 }
---
+++ test/Upstate.test.js
@@ -0,0 +1,81 @@
+const BigNumber = require('bignumber.js');
+require('@openzeppelin/test-helpers');
+const Upstate = artifacts.require('Upstate');
+require('chai')
+    .use(require('chai-bignumber')(BigNumber))
+    .should();
+const {
+    BN, // Big Number support
+    constants, // Common constants, like the zero address and largest integers
+    expectEvent, // Assertions for emitted events
+    expectRevert, // Assertions for transactions that should fail
+} = require('@openzeppelin/test-helpers');
+const time = require('@openzeppelin/test-helpers/src/time');
+
+contract('Upstate', accounts => {
+    const _name = 'Upstate';
+    const _symbol = 'Up';
+
+    const user1 = accounts[1];
+
+    let _supply = 1000;
+    const now = new Date();
+    const secondsSinceEpoch = Math.round(now.getTime() / 1000)
+    const _lesserDate = secondsSinceEpoch - 1000;
+    const _greaterDate = secondsSinceEpoch + 1000;
+
+    beforeEach(async function() {
+        this.owner = accounts[0];
+        this.token = await Upstate.new(_lesserDate, _greaterDate)
+        this.badToken = await Upstate.new(_lesserDate + 10000, _greaterDate - 100000)
+        await time.advanceBlock();
+    });
+
+    describe('basic token details', function() {
+        it('has correct name', async function() {
+            const name = await this.token.name();
+            name.should.equal(_name);
+        });
+
+        it('has correct symbol', async function() {
+            const symbol = await this.token.symbol();
+            symbol.should.equal(_symbol);
+        });
+
+        it('has 1000 total supply', async function() {
+            const supply = new BigNumber(await this.token.totalSupply());
+            supply.should.be.bignumber.equal(_supply);
+        });
+    })
+
+    describe('token transfer functionality', function() {
+        const to = accounts[4]
+        const amount = 100
+
+        it('correct starting balance', async function() {
+            const initialBalance = new BigNumber(await this.token.balanceOf(this.owner));
+            initialBalance.should.be.bignumber.equal(1000);
+        });
+
+
+        it('transfer OK within time bounds', async function() {
+            await this.token.increaseAllowance(to, amount);
+            await this.token.transfer(to, amount, { from: this.owner })
+            const senderBalance = new BigNumber(await this.token.balanceOf(this.owner))
+            senderBalance.should.be.bignumber.equal(900);
+            const recipientBalance = new BigNumber(await this.token.balanceOf(to))
+            recipientBalance.should.be.bignumber.equal(100);
+        });
+
+        it('transfer not OK below time bounds', async function() {
+            await this.badToken.increaseAllowance(to, amount);
+            await this.badToken.transfer(to, amount, { from: this.owner })
+            const senderBalance = new BigNumber(await this.token.balanceOf(this.owner))
+            senderBalance.should.be.bignumber.equal(900);
+            const recipientBalance = new BigNumber(await this.token.balanceOf(to))
+            recipientBalance.should.be.bignumber.equal(100);
+        });
+
+
+    })
+})
